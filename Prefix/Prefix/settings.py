21be4088fc6f26a999dc61bc4864905ec0668446
shirshanag <15shirsha@gmail.com>
2026-04-28 22:29:58 +0530

    Make build.sh executable

--- Prefix/Prefix/settings.py
+++ Prefix/Prefix/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&v9u+4c2ar$_90747w(06)slx=1$*(ww-p@vg#!+&avji80hw7'
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
