c37ecbd091abb9e52b3af18c3c78e5cb3b09c4be
shirshanag <15shirsha@gmail.com>
2026-04-28 23:01:07 +0530

    Add route url for homepage

--- Prefix/Prefix/urls.py
+++ Prefix/Prefix/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="home"),
     path('index/',views.index,name="index"),
     path('about/',views.about,name="about"),
     path('contact/',views.contact,name="contact"),
