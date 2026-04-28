863d4a8ba447c37ca962f1fe010baebc7edfec60
shirshanag <15shirsha@gmail.com>
2026-04-28 22:00:37 +0530

    Initial deploy

--- Prefix/Prefix/settings.py
+++ Prefix/Prefix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&v9u+4c2ar$_90747w(06)slx=1$*(ww-p@vg#!+&avji80hw7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,5 +124,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS=[
     BASE_DIR,'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
