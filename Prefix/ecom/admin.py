d72403766f2eb7c25c1d72c5fc2f7ea09f5dcb02
shirshanag <15shirsha@gmail.com>
2026-04-29 11:57:22 +0530

    Modifying admin.py

--- Prefix/ecom/admin.py
+++ Prefix/ecom/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Product
-
-
+from .models import user_data
+from .models import contact_data
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(user_data)
+admin.site.register( contact_data)
